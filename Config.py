416a7b7d80ef976b33901c687c82ca15dc856c69
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2023-10-26 01:00:21 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("APP_ID", "6435225"))
     API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6127476417:AAFwCcnDziasXrdRCad7dyhaZz2joA6Kd-o")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6392016724:AAGVd5l1161SSymqZQLTckUfMOpEGLh67Y8")
     STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOJQBu2Ig6ankQ_EZBGl-7IkV8svybov4S8QV16FHkmR9JxHUTRhFltIdz7qWREV9ox3rzFDd33fqkcZJp5e82PF1t8LYShVd30tqBFYm5VL2LvRH-h9ugp1lXPvRfPmcPDiP6vpIZwGnixOpguhogQZrisdOe0zlBJs4qfFQos7FNC8XJXgOeD8WRU9owkEBM2Pqs68YBx3zdR4fuMMRlrVVdLh3TvObQPL1e29ja1Shn4LCQgqWv5XIGt1CkGZFvic4hXykbQVDctQrHBXPTFZYpXW_uAkPuLllBasOFY8Wta4Qd5NS_lkMmxFlG7rNMWiNezbQTDbT49E5EU8OHy0UEs4=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", True)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
